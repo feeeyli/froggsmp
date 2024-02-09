2cee88716ae5a6d7ab0477f4c34fd30dccee535c
Luna Santos <lunafeyli@gmail.com>
2024-02-09 14:59:24 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,12 @@ import { StreamSchema } from "@/types/stream.schema";
 import Link from "next/link";
 
 export default async function Home() {
-  const streams: StreamSchema[] = [];
-
-  console.log(
-    await fetch(process.env.NEXT_PUBLIC_BASE_API_URL + "/api/streams", {
+  const streams: StreamSchema[] = await fetch(
+    process.env.NEXT_PUBLIC_BASE_API_URL + "/api/streams",
+    {
       next: { revalidate: 30 },
-    }).then((res) => res.json())
-  );
+    }
+  ).then((res) => res.json());
 
   const events = await getEvents();
   const updates = await getUpdates();
