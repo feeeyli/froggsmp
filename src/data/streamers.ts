16bfe5f2997d64605f1d42429a224599126537ae
Luna Santos <lunafeyli@gmail.com>
2024-03-31 22:41:14 -0300

    el pitchula

--- src/data/streamers.ts
+++ src/data/streamers.ts
@@ -2,7 +2,292 @@
 
 import { StreamerSchema } from "@/types/streamer.schema";
 
-export const STREAMERS: StreamerSchema[] = [
+const PRIMEIRO_DE_ABRIL = [
+    {
+        "display_name": "Yumithra",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/5d465f44-69bf-4b06-b49f-b20944fe0bcf-profile_image-300x300.png",
+        "minecraft_uuid": "46b47da6-fd09-4be9-beba-d64717eadb7f",
+        "twitch_login": "scottonauta",
+        "twitter_login": "scottonauta",
+        "youtube_login": "Scortes",
+        "characters": [
+            {
+                "name": "Scott",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Scottonauta"
+            },
+            {
+                "name": "Abaddon",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Abbadon"
+            }
+        ]
+    },
+    {
+        "display_name": "Mynzinha",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/4faa84a8-2b12-4123-9b0e-61d471cacf8b-profile_image-300x300.png",
+        "minecraft_uuid": "180ac11e-7beb-4322-9a55-17c26293723d",
+        "twitch_login": "umildlive",
+        "twitter_login": "UmildCDU",
+        "youtube_login": "umildlive",
+        "characters": [
+            {
+                "name": "Roberto Goodman",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Umild#Umild_Robert_Goodman"
+            },
+            {
+                "name": "Pierre Pintafacil",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Umild#Pierre_PintaF%C3%A1cil"
+            }
+        ]
+    },
+    {
+        "display_name": "Tiba",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/7b4253a0-eb8a-4f5e-8ea2-1b715c6c39b7-profile_image-300x300.png",
+        "minecraft_uuid": "79c50e8f-32a2-452d-89c6-f2574149f63f",
+        "twitch_login": "tiba041",
+        "twitter_login": "tiba041",
+        "youtube_login": "TibaLives",
+        "characters": [
+            {
+                "name": "Tiba",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Tibbazin"
+            }
+        ]
+    },
+    {
+        "display_name": "Febatista",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/69a4e8bc-01be-4f11-95ab-1c949932a13a-profile_image-300x300.png",
+        "minecraft_uuid": "f6a94a1e-9fc9-42b0-bc87-0ac8f4866590",
+        "twitch_login": "kellerzons",
+        "twitter_login": "kellerzons",
+        "youtube_login": "Bilaw",
+        "characters": [
+            {
+                "name": "Keller",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Kellerzons"
+            }
+        ]
+    },
+    {
+        "display_name": "Rafly",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/e08bd224-64d0-4f1f-8473-1ff7b1dcf195-profile_image-300x300.png",
+        "minecraft_uuid": "46fa9dec-08a2-4722-bd59-00f190cab20a",
+        "twitch_login": "oakinoo",
+        "twitter_login": "oAkinooo",
+        "youtube_login": "oakinoo",
+        "characters": [
+            {
+                "name": "Akino",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Akino"
+            }
+        ]
+    },
+    {
+        "display_name": "Keller",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/7cee8027-2e00-4c58-924f-7bc30bf90e0c-profile_image-300x300.png",
+        "minecraft_uuid": "ea00118d-fed9-4300-9bae-d1bb0cd83648",
+        "twitch_login": "ameizim",
+        "twitter_login": "ameizim",
+        "youtube_login": "AmeizimLive",
+        "characters": [
+            {
+                "name": "Ameizim",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Ameizim"
+            }
+        ]
+    },
+    {
+        "display_name": "Carrasquera",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/f2614f03-9849-478f-8110-ab9adf8a8531-profile_image-300x300.png",
+        "minecraft_uuid": "57d98d0a-2ee3-47e2-ae14-eb32185bc45e",
+        "twitch_login": "bastet",
+        "twitter_login": "bastet_min",
+        "youtube_login": "Bastet_min",
+        "characters": [
+            {
+                "name": "Bastet",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Bastet"
+            }
+        ]
+    },
+    {
+        "display_name": "FehDubs",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/1917209e-3c4e-49c2-8c09-e4e77141e1e1-profile_image-300x300.png",
+        "minecraft_uuid": "79c29788-6a07-440e-95ce-ccc63a0644f6",
+        "twitch_login": "carrasquera",
+        "twitter_login": "Carras_quera",
+        "youtube_login": "CarrasqueraLives",
+        "characters": [
+            {
+                "name": "Carrasquera",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Carrasquera"
+            }
+        ]
+    },
+    {
+        "display_name": "JinkiWinkki",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/441d9598-0dc8-4dc9-bad0-e5b03e017ffb-profile_image-300x300.png",
+        "minecraft_uuid": "70999ec8-234b-4ae6-bfb3-0304b698ea07",
+        "twitch_login": "dreasbro",
+        "twitter_login": "DreasBro",
+        "youtube_login": "LivesDoDreas",
+        "characters": [
+            {
+                "name": "DreasBro",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Dreasbro"
+            }
+        ]
+    },
+    {
+        "display_name": "Flopi",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/031207fe-1ebb-4d1f-b406-27553e0ecb83-profile_image-300x300.png",
+        "minecraft_uuid": "aab947c7-307a-4171-9f98-19ef68379c49",
+        "twitch_login": "febatista",
+        "twitter_login": "Febatista",
+        "youtube_login": "FebatistaLives",
+        "characters": [
+            {
+                "name": "Febatista",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Febatista"
+            }
+        ]
+    },
+    {
+        "display_name": "Kaory",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/fee53afc-157d-412e-8761-6f2f3dd741ef-profile_image-300x300.png",
+        "minecraft_uuid": "5775b22b-b75b-432a-b1a2-29d3d5ce7448",
+        "twitch_login": "fehdubs",
+        "twitter_login": "fehdubs",
+        "youtube_login": "FehDubs",
+        "characters": [
+            {
+                "name": "FehDubs",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/%C3%81ries"
+            }
+        ]
+    },
+    {
+        "display_name": "DreasBro",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/23d2b93f-4e22-4724-a494-266310c8fd71-profile_image-300x300.png",
+        "minecraft_uuid": "8d04f1c4-08f1-4d88-bc86-8ac55b55c314",
+        "twitch_login": "oflopi",
+        "twitter_login": "oflopi_",
+        "youtube_login": "oflopinho",
+        "characters": [
+            {
+                "name": "Flopi",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Oflopi"
+            }
+        ]
+    },
+    {
+        "display_name": "Bastet",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/0129c7d8-8e76-4501-8bad-35e6c0d3c0f6-profile_image-300x300.png",
+        "minecraft_uuid": "9fb2c3ae-5bab-4773-8cf2-5ece58d65748",
+        "twitch_login": "jinkiwinkki",
+        "twitter_login": "jinkiwinkki",
+        "youtube_login": "livesdajinkiwinkki",
+        "characters": [
+            {
+                "name": "JinkiWinkki",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Jinkiwinkki"
+            }
+        ]
+    },
+    {
+        "display_name": "Ameizim",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/b2784187-3ee7-4c31-8161-59ac6fddda42-profile_image-300x300.png",
+        "minecraft_uuid": "f084ae9b-c667-491d-8729-424c048c8076",
+        "twitch_login": "kaaory",
+        "twitter_login": "imkaory",
+        "youtube_login": "livesdakaory",
+        "characters": [
+            {
+                "name": "Kaory",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Kaaory"
+            }
+        ]
+    },
+    {
+        "display_name": "Akino",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/634e48a5-bcbb-408e-81ba-b2b7666931f5-profile_image-300x300.png",
+        "minecraft_uuid": "87720615-8a89-41af-9434-f32aa0416b86",
+        "twitch_login": "kojjlul",
+        "twitter_login": "Kojjlul",
+        "youtube_login": "ratariavods",
+        "characters": [
+            {
+                "name": "Kojj",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Kojj"
+            }
+        ]
+    },
+    {
+        "display_name": "Scott",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/41d41923-67ac-4498-9df5-e4d38f46f96f-profile_image-300x300.png",
+        "minecraft_uuid": "e72ad480-fbde-4501-98bb-e2da9579b045",
+        "twitch_login": "ljoga",
+        "twitter_login": "LJoga",
+        "youtube_login": "LJogaLives",
+        "characters": [
+            {
+                "name": "Paulo Reporter",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/LJoga"
+            },
+            {
+                "name": "LJoga",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/LJoga"
+            }
+        ]
+    },
+    {
+        "display_name": "Umild",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/fa41da80-e980-4bb6-a580-bd6ea487b14f-profile_image-300x300.png",
+        "minecraft_uuid": "703196c7-4c8d-4c22-bde8-601a08498a9d",
+        "twitch_login": "mynluvsx",
+        "twitter_login": "mynluvsx",
+        "youtube_login": "livesdamyn",
+        "characters": [
+            {
+                "name": "Mynzinha",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Mynzinha"
+            }
+        ]
+    },
+    {
+        "display_name": "LJoga",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/8ebe8da4-c266-4809-92bb-605331a29627-profile_image-300x300.png",
+        "minecraft_uuid": "b0bb3c43-de47-4013-bc7b-996f4819d2c7",
+        "twitch_login": "imrafly",
+        "twitter_login": "imraflying",
+        "youtube_login": "livesdarafly",
+        "characters": [
+            {
+                "name": "Rafly",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Rafly"
+            }
+        ]
+    },
+    {
+        "display_name": "Kojj",
+        "avatar_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/89dba2f2-567d-4697-840f-1c3cc863e7f0-profile_image-300x300.png",
+        "minecraft_uuid": "58bc10e6-93b1-4b5a-b936-cf43248c7af2",
+        "twitch_login": "yuyusaz",
+        "twitter_login": "YuyuSaz",
+        "youtube_login": null,
+        "characters": [
+            {
+                "name": "Yumithra",
+                "wiki_url": "https://frogg-smp.fandom.com/pt-br/wiki/Yumithra"
+            }
+        ]
+    }
+]
+
+export const STREAMERS: StreamerSchema[] = (((() => {
+    const brazilTimezoneOffset = -3; // Fuso horário do Brasil (UTC-3) em horas
+    const brazilTime = new Date(Date.now() + brazilTimezoneOffset * 60 * 60 * 1000); // Ajuste para o fuso horário do Brasil
+    return brazilTime.getDate(); // Retorna o dia do mês
+})() === 1) || false) ? PRIMEIRO_DE_ABRIL : [
   {
     display_name: "Scott",
     avatar_url:
